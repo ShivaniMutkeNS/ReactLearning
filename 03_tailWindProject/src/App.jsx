d5790d4960dbe8566bcaa0ab9e25af84ca27b390
shivanimutkeNS <shivani.mutke@noovosoft.com>
2024-02-24 13:19:59 +0530

    4 projects in one

--- 03_tailWindProject/src/App.jsx
+++ 03_tailWindProject/src/App.jsx
@@ -2,6 +2,9 @@ import {useState} from 'react'
 import './App.css'
 import Card from "./Components/Cards.jsx";
 import Card2 from "./Components/Card2.jsx";
+import BgChangerApp from "./Components/bg-changer"
+import PassWordGenApp from "./Components/password-generator.jsx"
+import Project from "./Components/projects.jsx";
 function App() {
     let obj ={
         user:"Shivani",
@@ -13,9 +16,12 @@ function App() {
         <>
             <h1 className='bg-purple-400 text-black p-4 rounded-xl'>Lets Do code with chai</h1>
             <Card channel ="Chai Or CODE" object={arr} object2={obj}></Card>
-
             <Card2 name="Shivani" profession="Full-Stack Developer"></Card2>
-            <Card2 name="Shivani"></Card2>
+            <Project></Project>
+            {/*<Card2 name="Shivani"></Card2>*/}
+            <PassWordGenApp></PassWordGenApp>
+            <BgChangerApp></BgChangerApp>
+
         </>
     )
 }
